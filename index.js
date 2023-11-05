1d43354cfa0c1f1b64adb82b02a7d659f7c360e0
sohan0898 <shuhanur.rahman08@gmail.com>
2023-11-05 15:03:49 +0600

    basic server setup completed

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const ports = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+// routes
+
+app.get('/', (req, res) => {
+res.send('FoodFeast Server Started');
+});
+
+app.listen(ports, () => {
+console.log( `FoodFeast server running on port ${ports}`);
+});
