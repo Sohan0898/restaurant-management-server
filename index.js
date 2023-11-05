586acd3ce147868ae2126f4edfc4535e2353541e
sohan0898 <shuhanur.rahman08@gmail.com>
2023-11-05 15:15:10 +0600

    connet to mongodb successfully

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
 const app = express();
 const ports = process.env.PORT || 5000;
 
@@ -9,12 +10,45 @@ app.use(cors());
 app.use(express.json());
 
 
+// mongoDB
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8p2aqm7.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
 // routes
 
 app.get('/', (req, res) => {
-res.send('FoodFeast Server Started');
+res.send('FoodieFeast Server Started');
 });
 
 app.listen(ports, () => {
-console.log( `FoodFeast server running on port ${ports}`);
+console.log( `FoodieFeast server running on port ${ports}`);
 });
