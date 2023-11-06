08acc6d5b79f85c7faf6cece2cdb712cdeccd438
sohan0898 <shuhanur.rahman08@gmail.com>
2023-11-06 11:23:34 +0600

    my added product are store in DB by Post method

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const ports = process.env.PORT || 5000;
@@ -9,13 +9,9 @@ const ports = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 // mongoDB
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8p2aqm7.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,16 +19,27 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
+    const FoodCollection = client.db("FoodDB").collection("myAddedFood");
+
+    app.post("/myAddedFood", async (req, res) => {
+      const newFood = req.body;
+      console.log(newFood);
+      const result = await FoodCollection.insertOne(newFood);
+      res.send(result);
+    });
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -40,15 +47,12 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
 // routes
 
-app.get('/', (req, res) => {
-res.send('FoodieFeast Server Started');
+app.get("/", (req, res) => {
+  res.send("FoodieFeast Server Started");
 });
 
 app.listen(ports, () => {
-console.log( `FoodieFeast server running on port ${ports}`);
+  console.log(`FoodieFeast server running on port ${ports}`);
 });
