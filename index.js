e0124aba2f73c829bee94b3b57aa0451af2006ff
sohan0898 <shuhanur.rahman08@gmail.com>
2023-11-07 10:56:46 +0600

    delete the ordered food by id using params

--- index.js
+++ index.js
@@ -50,7 +50,13 @@ async function run() {
         res.send(result);
       });
 
-      
+      app.delete('/orderedFood/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await OrderCollection.deleteOne(query);
+        res.send(result);
+ 
+     })
 
     // added food
     app.get("/myAddedFood", async (req, res) => {
